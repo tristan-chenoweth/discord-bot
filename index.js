9b7c7c8f40927ecf2b8974dace65ea51defab4c6
tristan-chenoweth <tristan.chenoweth@outlook.com>
2022-05-25 07:24:28 -0500

    update

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 // Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.commands = new Collection();
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
