e3f9d5374ddc9174ed1e403cbc39210e82d5fbcf
tristan-chenoweth <tristan.chenoweth@outlook.com>
2022-05-29 17:22:53 -0500

    update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Require the necessary discord.js classes
 const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
+let date;
+
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
@@ -10,11 +12,27 @@ client.commands = new Collection();
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
 	console.log('Ready!');
+//	setInterval(() => { console.log(date = new Date); }, 2000);
 });
 
 client.on('messageCreate', (message) => {
 	if (message.content.startsWith('$ping')) {
 		message.channel.send('pong!');
+	}
+	if (message.content.startsWith('$hello')) {
+		message.channel.send('Hi');
+	}
+	if (message.content.startsWith('$xur')) {
+		date = new Date;
+		if (date.getDay == 5 || date.getHours == 17 || date.getMinutes == 30) {
+			client.users.get('Charlemagne#3214').send('/xur');
+		}
+		else {
+			message.channel.send('Xur is Communing with The Nine.');
+		}
+	}
+	if (message.content.startsWith('$help')) {
+		message.channel.send('Here are the available commands. \n $ping - responds with "pong!" \n $hello - resonds with Hi \n $ Xur with respond with information on Xurs location');
 	}
 });
 
